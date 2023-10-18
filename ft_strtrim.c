b16b294b18be32a772e46381b984f62955399e43
Dew-Hub <burakonturk2000@gmail.com>
2023-10-18 13:40:33 +0300

    .

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,23 +6,46 @@
 /*   By: buonturk <buonturk@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 16:34:23 by buonturk          #+#    #+#             */
-/*   Updated: 2023/10/17 16:45:20 by buonturk         ###   ########.fr       */
+/*   Updated: 2023/10/18 11:13:02 by buonturk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	checker(char c, char const *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i++] == c)
+			return (1);
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
-	size_t	c;
+	int		i;
+	int		j;
+	int		k;
 
-	c = 0;
-	while (set[c])
-	{
-		if(s1[0] == set[0])
-			
-		c++;
-	}
-	
+	i = 0;
+	k = 0;
+	if (s1 == 0 || set == 0)
+		return (NULL);
+	j = ft_strlen(s1);
+	while (s1[i] && checker(s1[i], set))
+		i++;
+	while (j > i && checker(s1[j - 1], set))
+		j--;
+	str = (char *)malloc(sizeof(char) * (j - i + 1));
+	if (!str)
+		return (NULL);
+	while (i < j)
+		str[k++] = s1[i++];
+	str[k] = '\0';
+	return (str);
 }
